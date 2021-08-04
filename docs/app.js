a787e8e61bea91b52ebda4353c363adc79988ce8
Duhan Bayrak <duhanbayrak@Duhan-MacBook-Air.local>
2021-08-04 22:16:37 +0300

    first commit

---
+++ docs/app.js
@@ -0,0 +1,70 @@
+let eczAd = [];
+let eczAdrs = [];
+let eczTel = [];
+let eczSehir = [];
+let eczIlce = [];
+
+var tarih = new Date();
+
+var gunler = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"];
+var day = tarih.getDate();
+var month = tarih.getMonth();
+var year = tarih.getFullYear();
+
+if (day < 9) {
+    day = "0" + tarih.getDate();
+}
+if (month < 9) {
+    month = "0" + tarih.getMonth();
+}
+
+document.getElementById("date_").innerHTML += `
+
+    <h1>${day}.${month}.${year} ${gunler[tarih.getDay()]}</h1>
+   
+`;
+function getEcz() {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "dataset/ecz.json", true); //asi.json dosyasını açıyoruz.
+
+    xhr.onload = function () {
+        if (this.status === 200) {
+
+
+            let eczJson = JSON.parse(this.responseText);
+            let eczInfo = eczJson.data;
+            let tplmEcz = eczJson.rowCount;
+
+            eczInfo.forEach(e => {
+                eczAd.push(e.EczaneAdi);
+                eczAdrs.push(e.Adresi);
+                eczTel.push(e.Telefon);
+                eczSehir.push(e.Sehir);
+                eczIlce.push(e.ilce);
+            });
+
+
+            for (let i = 0; i < tplmEcz; i++) {
+
+                document.getElementById("eczTblBody").innerHTML += `
+                
+                    <td id="eczAd"><img id="eczLogo" src="/img/ecz.png" alt="Medikar" srcset=""> ${eczAd[i]}</td>
+                    
+                    <td id="eczAdrs"> <i class="fas fa-map-marker-alt"></i> ${eczAdrs[i]}</td>
+                    
+                    <td id="eczTel"> <i class="fas fa-phone"></i> ${eczTel[i]}</td>
+                    
+                    <td id="eczIlce">${eczIlce[i]}</td>
+    
+                `
+            }
+
+
+
+        }
+    };
+    xhr.send();
+}
+
+getEcz();
---
+++ index.js
@@ -0,0 +1,36 @@
+const http = require('http');
+var cron = require('node-cron');
+const express = require("express");
+const fs = require('fs');
+var axios = require('axios');
+var qs = require('qs');
+const console = require('console');
+var data = qs.stringify({
+
+});
+
+cron.schedule("*/1 * * * *", function () {
+
+    axios(config)
+        .then(function (response) {
+            console.log(JSON.stringify(response.data));
+            varDataEcz = JSON.stringify(response.data);
+            fs.writeFileSync("docs/dataset/ecz.json", varDataEcz);
+
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+})
+
+
+var config = {
+    method: 'get',
+    url: 'https://www.nosyapi.com/apiv2/pharmacy?city=karabuk',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer 47gch9lETlpp44KnOcHnuVy5VYKhy84tosYVbAlkq3FYBmvvTXkfvQZaLHQ9'
+    },
+    data: data
+};
+

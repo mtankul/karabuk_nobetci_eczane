866b4650325d66df59bacb4eaa459af78e369e9e
Duhan Bayrak <47924611+duhanbayrak@users.noreply.github.com>
2021-08-04 23:12:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,19 +18,17 @@ var config = {
     data: data
 };
 
-cron.schedule("*/1 * * * *", function () {
-
     axios(config)
         .then(function (response) {
-            console.log(JSON.stringify(response.data));
-            varDataEcz = JSON.stringify(response.data);
-            fs.writeFileSync("docs/dataset/ecz.json", varDataEcz);
+        console.log(JSON.stringify(response.data));
+        varDataEcz = JSON.stringify(response.data);
+        fs.writeFileSync("docs/dataset/ecz.json", varDataEcz);
 
         })
         .catch(function (error) {
             console.log(error);
         });
-})
+
 
 
 

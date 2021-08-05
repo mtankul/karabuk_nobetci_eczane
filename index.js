12c7b0566264465fb2ba017873719151ce2aa0ac
Duhan Bayrak <47924611+duhanbayrak@users.noreply.github.com>
2021-08-05 08:21:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var config = {
         .then(function (response) {
         console.log(JSON.stringify(response.data));
         varDataEcz = JSON.stringify(response.data);
-        fs.writeFile("docs/dataset/ecz.json", varDataEcz);
+        fs.writeFile("docs/dataset/ecz.json", varDataEcz,null,4);
 
         })
         .catch(function (error) {

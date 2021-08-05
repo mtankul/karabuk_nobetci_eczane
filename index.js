a137ebc51a74b00faa46ff06bc08cedf9cc90100
Duhan Bayrak <47924611+duhanbayrak@users.noreply.github.com>
2021-08-05 09:29:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function veriCek() {
     axios(config)
         .then(function (response) {
             console.log(JSON.stringify(response.data));
-            varDataEcz = JSON.stringify(response.data);
+            varDataEcz = JSON.stringify(response.data,null,4);
             fs.writeFileSync("docs/dataset/ecz.json", varDataEcz);
     
         })
